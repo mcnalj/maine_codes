b25222f88aa322666d4f9d030e3608f3dc8e7e23
mcnalj <example@example.com>
2021-08-18 12:20:31 -0400

    modified static_root

--- maine_codes/settings.py
+++ maine_codes/settings.py
@@ -146,7 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_URL = '/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/dh_w223p2/mainecodes.com/public/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
